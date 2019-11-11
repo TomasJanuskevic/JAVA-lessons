e6a05ebfe312771bb18b8a3566b2ad360a4b52fe
Tomas <tomjanuskevic@gmail.com>
2019-11-11 20:45:56 +0200

    First commit

---
+++ src/HomeWork/Lesson1.java
@@ -0,0 +1,16 @@
+
+package HomeWork;
+
+public class Lesson1 {
+    
+    public static void main(String[] args) {
+        System.out.println("               Saulei tekant");
+        System.out.println("Jau saulėtekis mėlsvas, spinduliais iškaišytas,");
+        System.out.println("Tvinksta, rausta ir dega lyg veideliai mergaičių,");
+        System.out.println("Kai po langu sužvengia žirgas jauno bernaičio.");
+        System.out.println("Štai ir saulė jau teka. Sveikas, vasaros rytas!");
+        System.out.println("                                      Maironis");
+    }
+
+}
+
---
+++ src/Lesson2/PrimitiveDataTypes.java
@@ -0,0 +1,47 @@
+
+package Lesson2;
+
+public class PrimitiveDataTypes {
+    public static void main(String [] args) {
+        byte b1 = 10;
+        byte b2 = 100;
+        
+        short s1 = 130;
+        
+        int il = 500;
+        
+        long l1 = 100000000000L;
+        
+        float f1 = 3.14F;
+        
+        double d1 = 3.14;
+        
+        char c1 = 'a';
+        char c2 = 'A';
+        char c3 = '7';
+        char c4 = ' ';
+        char c5 = 100;
+        char c6 = '\u2abc';
+        
+        boolean bool1 = true;
+        boolean bool2 = false;
+        
+        int a1 = 60;
+        
+        int a2 = 0B111100;
+        int a3 = 074;
+        int a4 = 0X3C;
+        
+        int a5 = 1_000_000;
+        
+        System.out.println(f1);
+        System.out.println(c5);
+        System.out.println(c6);
+        System.out.println(bool1);
+        System.out.println(a4);
+        System.out.println(a5);
+        
+        
+    }
+    
+}
