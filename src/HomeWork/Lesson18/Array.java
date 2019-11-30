fb90980ad7bcdc2b58c127bd264a240c9a18fcad
Tomas <tomjanuskevic@gmail.com>
2019-11-30 14:48:31 +0200

    Change HomeWork18

--- src/HomeWork/Lesson18/Array.java
+++ src/HomeWork/Lesson18/Array.java
@@ -3,26 +3,31 @@ package HomeWork.Lesson18;
 public class Array {
 
     static int[] array(int[] array) {
-        int min = array[0];
-
+        int a;
         for (int i = 0; i < array.length; i++) {
-
-            for (int j = 0; j < array.length; j++) {
+            int min = array[i];
+            int index = i;
+            for (int j = i + 1; j < array.length; j++) {
                 if (array[j] < min) {
                     min = array[j];
+                    index = j;
                 }
             }
-            array[i] = min;
+            if (i != index) {
+                a = array[i];
+                array[i] = min;
+                array[index] = a;
+            }
         }
         return array;
     }
 
     public static void main(String[] args) {
-        int[] ar1 = {2, 6, 4, -5, 50};
+        int[] ar1 = {9, 4, -5, 50, 9};
         array(ar1);
 
         for (int a : ar1) {
-            System.out.print(a + "");
+            System.out.print(a + " ");
         }
     }
 
