de45e1d40a34eb6519af1853b9dd9d31d26bb7e8
Tomas <tomjanuskevic@gmail.com>
2019-11-30 15:28:56 +0200

    HomeWork19

---
+++ src/HomeWork/Lesson19/Arrays.java
@@ -0,0 +1,27 @@
+package HomeWork.Lesson19;
+
+public class Arrays {
+
+    static String[] abc(String[]... array) {
+        int length = 0;
+        for (String[] array1 : array) {
+            length += array1.length;
+        }
+        String[] target = new String[length];
+        int count = 0;
+        for (String[] array1 : array) {
+            for (String s : array1) {
+                target[count] = s;
+                count++;
+            }
+        }
+        return target;
+    }
+
+    public static void main(String[] args) {
+        String[] Array = abc(new String[]{"a", "b", "c"}, new String[]{"d", "e"}, new String[]{"f"});
+for(String s: Array){
+    System.out.print(s + " ");
+    }
+    }
+}
